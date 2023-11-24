632e8bb0e0cbee4f54a83a0d62c75dbf8480b850
DudsS2 <edus.barbosa16@gmail.com>
2023-11-23 21:28:27 -0300

    Recuperar Senha
    e começo do feed deslogado, tmb organizei um pouco dos arquivos

---
+++ Front/Feed deslogado/Feed deslogado.js
@@ -0,0 +1,46 @@
+const criaposts = [
+    {
+        id: 1,
+        title: "Post 1",
+        content: "Não se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandia"
+    },
+    {
+        id: 2,
+        title: "Post 2",
+        content: "Não se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandia"
+    },
+    {
+        id: 3,
+        title: "Post 3",
+        content: "Não se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandia"
+    },
+    {
+        id: 4,
+        title: "Post 4",
+        content: "Não se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandia"
+    },
+    {
+        id: 5,
+        title: "Post 5",
+        content: "Não se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandia"
+    },
+    
+]
+
+const renderpost = () => {
+    const postconteiner = document.querySelector(".lista-de-post")
+
+    criaposts.forEach(post =>{
+        const postElement = document.createElement('div')
+        postElement.classList.add("post")
+        
+        postElement.innerHTML = `
+            <h2>${post.title}</h2>
+            <p>${post.content}</p>
+        `
+        postconteiner.appendChild(postElement)
+    })
+
+}
+
+renderpost()
