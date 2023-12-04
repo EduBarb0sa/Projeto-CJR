317950eebfd3a3da1e323774d326fd8bbc4778b6
DudsS2 <edus.barbosa16@gmail.com>
2023-12-04 15:59:59 -0300

    JS do modal
    ...

---
+++ Front/Perfil-Logado/modal.js
@@ -0,0 +1,76 @@
+const criaposts = [
+    {
+        id: 1,
+        title: "Post 1",
+        content: "Não se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandia"
+    },
+    {
+        id: 2,
+        title: "Post 2",
+        content: "Não se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandia"
+    },
+    {
+        id: 3,
+        title: "Post 3",
+        content: "Não se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandia"
+    },
+    {
+        id: 4,
+        title: "Post 4",
+        content: "Não se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandia"
+    },
+    {
+        id: 5,
+        title: "Post 5",
+        content: "Não se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandiaNão se fala de outra coisa no metro de ceilandia"
+    },
+    
+]
+const renderpost = () => {
+    const postconteiner = document.querySelector(".lista-de-post")
+    criaposts.forEach(post =>{
+        const postElement = document.createElement('div')
+        postElement.classList.add("post")
+        
+        postElement.innerHTML = `
+            <h2>${post.title}</h2>
+            <p>${post.content}</p>
+        `
+        postconteiner.appendChild(postElement)
+    })
+}
+renderpost()
+
+document.addEventListener('DOMContentLoaded', function () {
+    var simplemde = new SimpleMDE({ element: document.getElementById("markdownInput") });
+});
+
+function openModal() {
+    var dialog = document.getElementById('myModal');
+    dialog.showModal();
+}
+
+function closeModal() {
+    var dialog = document.getElementById('myModal');
+    dialog.close();
+}
+
+function submitPost() {
+    var markdownContent = document.getElementById("markdownInput").value;
+
+    fetch('url-do-seu-backend', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ content: markdownContent }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            closeModal();
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+        });
+}
