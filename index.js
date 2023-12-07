409af1056639ffc4dfbc4d5a8964911cf6d9c58a
cauetrindade <150065608+cauetrindade@users.noreply.github.com>
2023-12-06 21:43:15 -0300

    funcionando

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 import express from "express";
 import cors from "cors"
-import UserRouter from "./back/CRUD-user/users.controller.js";
-import postRouter from "./back/CRUD posts/posts.controller.js";
-import authRouter from "./back/auth/auth.controller.js";
-import path from "path"
-import { fileURLToPath } from "url";
-
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+import UserRouter from "./public/crud/CRUD-user/users.controller.js"
+import postRouter from "./public/crud/CRUD posts/posts.controller.js";
+import authRouter from "./public/crud/auth/auth.controller.js";
+import pagesRouter from "./rotas.js";
 
 const app = express()
 app.use(express.json())
@@ -19,18 +14,14 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
   });
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.use(pagesRouter)
 app.use(authRouter)
 app.use(cors())
 app.use(UserRouter)
 app.use(postRouter)
-app.get('/', (req,res) =>{
-    res.sendFile(path.join(__dirname,"/Front/Feed Logado/Feed Logado.html"))
-    res.sendFile(path.join(__dirname,  "/Front/Feed Logado/Feed Logado.css"))
-    res.sendFile(path.join(__dirname,  "/Front/Feed Logado/Feed Logado.js"))
-
-    
-})
-
 app.listen(8000, () => {
     console.log('Servidor rodando na url: http//:localhost:8000')
 })
