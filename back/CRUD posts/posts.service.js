712e0638fbb5d4237b3064e6e6e0395b257776d0
DudsS2 <edus.barbosa16@gmail.com>
2023-12-08 08:45:43 -0300

    Tirando conflito

---
+++ back/CRUD posts/posts.service.js
@@ -0,0 +1,57 @@
+/*import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient
+
+export default class Posts {
+    async createPost(userid, title,content) {
+        const userId = parseInt(userid)
+        return await prisma.posts.create({
+            data:{
+                userId: userId,
+                title: "pdokfpaok",
+                content: content
+            }
+        }).catch((e) =>{
+            if (e.code === 'P2002')
+                throw new Error('Usuário já cadastrado')
+            throw e
+        })
+
+    }
+    async getPosts(){
+        return await prisma.posts.findMany();
+        
+    }
+    async getUserPosts(userId){
+        console.log(userId)
+        return await prisma.posts.findMany({
+            where:{
+                userId: userId
+            }
+        })
+    }
+
+    async findById(id) {
+        try {
+            const postId = parseInt(id, 10)
+            const postFound = await prisma.posts.findUnique({
+                where: {
+                    id: postId
+                }
+            });
+            return postFound
+        }catch(error) {
+            console.error(error.message);
+            throw new Error("Erro ao buscar o post")
+        }
+    }
+
+    async deleteById(id) {
+        const postId = parseInt(id, 10)
+        return await prisma.posts.delete({
+            where: {
+                id: postId
+            }
+        })
+    }
+}*/
---
+++ back/CRUD-user/users.service.js
@@ -0,0 +1,89 @@
+/*import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default class Users {
+    async newUser(email, senha, nome, genero, cargo) {
+        return await prisma.user.create({
+            data: {
+                email: email,
+                senha: senha,
+                nome: nome,
+                genero: genero,
+                cargo: cargo,
+            }
+        }).catch((e) => {
+            if (e.code === 'P2002')
+                throw new Error('Usuário já cadastrado')
+            throw e
+        })
+
+    }
+
+    async listUsers() {
+        return await prisma.user.findMany();
+    }
+
+    async checkPassword(login, password) {
+        const userlogin = await prisma.user.findUnique({
+            where: { email: login }
+        })
+        if (userlogin == undefined) {
+            console.log('Usuário não cadastrado')
+            return 'Usuário não cadastrado'
+        }
+
+        if (userlogin.senha != password) {
+            console.log(userlogin.senha)
+            console.log(password)
+            return 'Senha não corresponde'
+
+        }
+        console.log('Login aceito')
+        return true
+    }
+
+    async changeInfo(email, info) {
+        const checkexist = await prisma.user.findUnique({
+            where: {
+                email: email
+            }
+
+        })
+        if (checkexist == null) {
+            return 'Usuário não existe'
+        }
+        const updateUser = await prisma.user.update({
+            where: {
+                email: email,
+            },
+            data: {
+                senha: info,
+            },
+        })
+        return "Senha alterada com sucesso"
+
+    }
+    async findByEmail(email) {
+        return await prisma.user.findUnique({
+            where: {
+                email: email
+            }
+        });
+    }
+    async findById(id) {
+        return await prisma.user.findUnique({
+            where: {
+                id: id
+            }
+        });
+    }
+
+    async deleteById(id) {
+        return await prisma.user.delete({
+            where: {
+                id: id
+            }
+        })
+    }
+}*/
